4a529566acb3411147e10d6df73bd0a9c3ac99d9
Tapple Gao <tapplek@gmail.com>
2017-05-31 15:07:21 -0500

    tutorial: use render() shortcut in index

--- djangoTutorial01/polls/views.py
+++ djangoTutorial01/polls/views.py
@@ -1,18 +1,14 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+#from django.template import loader
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)

57ad7ffd571685832afe9c34cb309d56b6d35680
Tapple Gao <tapplek@gmail.com>
2017-06-07 11:08:27 -0500

    tutorial: improved the django admin site

--- djangoTutorial01/djangoTutorial01/settings.py
+++ djangoTutorial01/djangoTutorial01/settings.py
@@ -57,6 +57,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',

f4138acd80a1ad1731f9575080f9a379b8bda082
Tapple Gao <tapplek@gmail.com>
2017-06-08 17:47:26 -0500

    add rest framework settings

--- www/www/settings.py
+++ www/www/settings.py
@@ -26,6 +26,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'delivery.apps.DeliveryConfig',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [

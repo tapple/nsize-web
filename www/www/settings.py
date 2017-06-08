9b5f1805c48bc0c69f6e79d6e45e0049a9c2e5b9
Tapple Gao <tapplek@gmail.com>
2017-06-08 17:38:57 -0500

    started main nsize api app, named delivery

--- www/www/settings.py
+++ www/www/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-from settings_secret import *
+from .settings_secret import *
 
 # Application definition
 

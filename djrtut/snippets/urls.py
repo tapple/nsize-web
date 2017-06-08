b98d4b3f6e9304ba1bd09d744ba48bfde1217aaf
Tapple Gao <tapplek@gmail.com>
2017-06-07 22:45:25 -0500

    tutorial: api is now browsable

--- djrtut/snippets/urls.py
+++ djrtut/snippets/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
@@ -6,3 +7,4 @@ urlpatterns = [
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
 
+urlpatterns = format_suffix_patterns(urlpatterns)

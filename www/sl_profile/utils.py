416aa865cfd794198f52dcf525066e7085cdbb98
Tapple Gao <tapplek@gmail.com>
2017-06-14 18:51:49 -0500

    added timeout and retry to the api for better reliability

--- www/sl_profile/utils.py
+++ www/sl_profile/utils.py
@@ -4,10 +4,21 @@ from uuid import UUID
 import re
 
 import requests
+import backoff
 from bs4 import BeautifulSoup
 
 SL_WORLD_BASE_URL = 'http://world.secondlife.com/resident/'
 SL_MARKETPLACE_BASE_URL = 'https://marketplace.secondlife.com'
+
+SESSION = requests.Session()
+
+@backoff.on_exception(backoff.expo,
+    (requests.exceptions.Timeout,
+    requests.exceptions.ConnectionError),
+    max_tries=3)
+def get_url(url):
+    r = SESSION.get(url, timeout=20)
+    return BeautifulSoup(r.text, "lxml")
 
 def find_avatar_key(name):
     """
@@ -15,8 +26,7 @@ def find_avatar_key(name):
     list of potential UUID's
     """
     url_re = re.compile(SL_WORLD_BASE_URL + '(.*)')
-    r = requests.get('http://search.secondlife.com/client_search.php?q=' + quote(name))
-    soup = BeautifulSoup(r.text, "html.parser")
+    soup = get_url('http://search.secondlife.com/client_search.php?q=' + quote(name))
     tags = soup('a', href=url_re)
     return [UUID(url_re.match(tag.attrs['href']).group(1)) for tag in tags]
 
@@ -26,8 +36,7 @@ def avatar_info(key):
     using the SL World API
     """
     Info = namedtuple("Info", 'fullName imgUrl description')
-    r = requests.get(SL_WORLD_BASE_URL + str(key))
-    soup = BeautifulSoup(r.text, "html.parser")
+    soup = get_url(SL_WORLD_BASE_URL + str(key))
     return Info(
         soup.find('title').string,
         soup.find('img', class_='parcelimg').attrs['src'],
@@ -43,8 +52,7 @@ def find_marketplace_store(name):
     Store = namedtuple("Store", 'url name')
     base_url = 'https://marketplace.secondlife.com'
     search_path = '/stores/store_name_search?search%5Bkeywords%5D='
-    r = requests.get(base_url + search_path + quote_plus(name))
-    soup = BeautifulSoup(r.text, "html.parser")
+    soup = get_url(base_url + search_path + quote_plus(name))
     tags = soup('a', href=re.compile('/stores/\d+'))
     return [Store(base_url + tag.attrs['href'], tag.string) for tag in tags]
 
@@ -53,8 +61,7 @@ def marketplace_product_info(url):
     returns the (name, image url, slurl) of the marketplace product at url
     """
     Info = namedtuple("Info", 'name imgUrl slurl')
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, "html.parser")
+    soup = get_url(url)
     img_tag = soup.select_one('a#main-product-image img')
     slurl_tag = soup.select_one('a.slurl')
     slurl = ""

abb4f34f16f050723915e842c8cbce94c282a859
Tapple Gao <tapplek@gmail.com>
2017-08-31 08:03:34 -0700

    Renamed sl_profile.utils to scraper

--- nsize/sl_profile/tests.py
+++ nsize/sl_profile/tests.py
@@ -3,68 +3,68 @@ import unittest
 
 from django.test import TestCase
 
-from . import utils
+from . import scraper
 
 class NetworkUtilsTestCase(unittest.TestCase):
     def test_find_avatar_key(self):
-        keys = utils.find_avatar_key('pippingao')
+        keys = scraper.find_avatar_key('pippingao')
         self.assertEqual(len(keys), 1)
         self.assertEqual(keys[0], UUID('79f70eef-cb35-42a9-bfcb-3ae85acd0331'))
 
     def test_avatar_info(self):
-        info = utils.avatar_info(UUID('79f70eef-cb35-42a9-bfcb-3ae85acd0331'))
+        info = scraper.avatar_info(UUID('79f70eef-cb35-42a9-bfcb-3ae85acd0331'))
         self.assertEqual(info.fullName, 'Pippin Gao (pippingao)')
         self.assertEqual(info.imgUrl, 'http://secondlife.com/app/image/8b1a0359-29e3-bbf0-b8eb-65af4080d2ae/1')
 
     def test_find_avatar_key(self):
-        stores = utils.find_marketplace_store('Tapple Gao')
+        stores = scraper.find_marketplace_store('Tapple Gao')
         self.assertEqual(len(stores), 1)
         self.assertEqual(stores[0].url, 'https://marketplace.secondlife.com/stores/62403')
         self.assertEqual(stores[0].name, 'Tavatar')
 
     def test_marketplace_store_info(self):
-        store = utils.marketplace_store_info('https://marketplace.secondlife.com/stores/185578/')
+        store = scraper.marketplace_store_info('https://marketplace.secondlife.com/stores/185578/')
         self.assertEqual(store.name, "MAMACITA & MADUSA"),
         self.assertEqual(store.slurl, "http://maps.secondlife.com/secondlife/Coral%20Coast/217/176/3812"),
         self.assertEqual(store.legacyName, "Y0URP0RN"),
         self.assertEqual(store.website, "http://y0urp0rn.tumblr.com/"),
 
     def test_marketplace_product_info_1(self):
-        info = utils.marketplace_product_info('https://marketplace.secondlife.com/p/FLG-No-Cabide-Ariana-Set-Top-Skirt-Shoes-HUDs-30-Models/11977233?ple=h')
+        info = scraper.marketplace_product_info('https://marketplace.secondlife.com/p/FLG-No-Cabide-Ariana-Set-Top-Skirt-Shoes-HUDs-30-Models/11977233?ple=h')
         self.assertEqual(info.name, '::FLG No Cabide - Ariana Set -Top-Skirt-Shoes-HUDs 30 Models')
         self.assertEqual(info.imgUrl, 'https://slm-assets2.secondlife.com/assets/17458054/view_large/FLG_No_Cabide_-_Ariana_Set_-_Top_-_Skirt_-_Shoes_-_HUDs_30_Models_M.jpg?1496927352')
         self.assertEqual(info.slurl, 'http://maps.secondlife.com/secondlife/Atlantico/73/210/22')
 
     def test_marketplace_product_info_2(self):
-        info = utils.marketplace_product_info('https://marketplace.secondlife.com/p/TentatioN-DEMO-Outfit-GUILE/8580808')
+        info = scraper.marketplace_product_info('https://marketplace.secondlife.com/p/TentatioN-DEMO-Outfit-GUILE/8580808')
         self.assertEqual(info.name, '* TentatioN * DEMO Outfit GUILE')
         self.assertEqual(info.imgUrl, 'https://slm-assets1.secondlife.com/assets/13448064/view_large/DEMO_Outfit.jpg?1455990153')
         self.assertEqual(info.slurl, '')
 
 class UtilsTestCase(unittest.TestCase):
     def test_to_username_1(self):
-        self.assertEqual(utils.to_username('Tapple Gao'), 'tapple.gao')
+        self.assertEqual(scraper.to_username('Tapple Gao'), 'tapple.gao')
 
     def test_to_username_2(self):
-        self.assertEqual(utils.to_username('tapple.gao'), 'tapple.gao')
+        self.assertEqual(scraper.to_username('tapple.gao'), 'tapple.gao')
 
     def test_to_fullname_1(self):
         self.assertEqual(
-                utils.to_fullname('tapple.gao', 'Tapple Gao'), 
+                scraper.to_fullname('tapple.gao', 'Tapple Gao'),
                 'Tapple Gao')
 
     def test_to_fullname_2(self):
         self.assertEqual(
-                utils.to_fullname('tapple.gao', 'Pippin Gao'),
+                scraper.to_fullname('tapple.gao', 'Pippin Gao'),
                 'Pippin Gao (tapple.gao)')
 
     def test_parse_fullname_1(self):
-        parsed = utils.parse_fullname('Tapple Gao')
+        parsed = scraper.parse_fullname('Tapple Gao')
         self.assertEqual(parsed.userName, 'tapple.gao')
         self.assertEqual(parsed.displayName, 'Tapple Gao'), 
 
     def test_parse_fullname_2(self):
-        parsed = utils.parse_fullname('Pippin Gao (tapple.gao)')
+        parsed = scraper.parse_fullname('Pippin Gao (tapple.gao)')
         self.assertEqual(parsed.userName, 'tapple.gao'),
         self.assertEqual(parsed.displayName, 'Pippin Gao'),
 
--- nsize/sl_profile/views.py
+++ nsize/sl_profile/views.py
@@ -1,14 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from . import utils
+from . import scraper
 
 class FindAvatarKey(APIView):
     """
     Search for named avatars and return a list of matching keys
     """
     def get(self, request, name, format=None):
-        keys = utils.find_avatar_key(name)
+        keys = scraper.find_avatar_key(name)
         return Response(keys)
 
 class AvatarInfo(APIView):
@@ -16,8 +16,8 @@ class AvatarInfo(APIView):
     Return full name, user name, display name, image url, description of the given uuid
     """
     def get(self, request, uuid, format=None):
-        info = utils.avatar_info(uuid)
-        names = utils.parse_fullname(info.fullName)
+        info = scraper.avatar_info(uuid)
+        names = scraper.parse_fullname(info.fullName)
         #return Response({**info._asdict(), **names._asdict()}) # Only in Python 3.5+
         response = info._asdict()
         response.update(names._asdict())
@@ -28,7 +28,7 @@ class FindMarketplaceStore(APIView):
     Search for the store for the given avatar name
     """
     def get(self, request, name, format=None):
-        stores = utils.find_marketplace_store(name)
+        stores = scraper.find_marketplace_store(name)
         return Response([store._asdict() for store in stores])
 
 class MarketplaceStoreInfo(APIView):
@@ -36,7 +36,7 @@ class MarketplaceStoreInfo(APIView):
     Search for the store for the given avatar name
     """
     def get(self, request, url, format=None):
-        store = utils.marketplace_store_info(url)
+        store = scraper.marketplace_store_info(url)
         return Response(store._asdict())
 
 class MarketplaceProductInfo(APIView):
@@ -44,6 +44,6 @@ class MarketplaceProductInfo(APIView):
     Search for the store for the given avatar name
     """
     def get(self, request, url, format=None):
-        info = utils.marketplace_product_info(url)
+        info = scraper.marketplace_product_info(url)
         return Response(info._asdict())
 
